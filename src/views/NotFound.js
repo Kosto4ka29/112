e24d68ae9bab2eaf206f8cb67fb12c318ebd7dcf
Kosto4ka29 <135865@student.uni.opole.pl>
2025-05-19 22:03:07 +0200

    styles

--- src/views/NotFound.js
+++ src/views/NotFound.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import PageTitle from '../components/PageTitle/PageTitle.js';
+ import PageTitle from '../components/PageTitle/PageTitle.js';
 
 const NotFound = () => {
   return (
